b3d2523c7044db279397aaef67f2166608e19ddc
nandimandalamrajeshreddy <rajeshreddy.nandi@gmail.com>
2020-08-18 20:02:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -42,14 +42,23 @@ variable "platform_fault_domain_count" {
 
 variable "vnet_address_range" {
     description =   "IP Range of the virtual network"
-    type        =    string
-    default     =   "10.0.0.0/16"
+    type        =    map(string)
+    default         =       {
+        "name"              =       "Dev-Vnet"
+        "address_range"     =       "10.0.0.0/16"
+    }
 }
+
+
 
 variable "subnet_address_range" {
     description =   "IP Range of the virtual network"
-    type        =    string
-    default     =   "10.0.1.0/24"
+    type            =       map(string)
+    default         =       {
+        "Web-Subnet"    =       "10.0.1.0/24"
+        "App-Subnet"    =       "10.0.2.0/24"
+        "DB-Subnet"     =       "10.0.3.0/24"
+    }
 }
 
 # Public IP and NIC Allocation Method
