ca7b7ba1ff33363473fd413e3044d4c04ab754d8
nandimandalamrajeshreddy <rajeshreddy.nandi@gmail.com>
2020-08-18 20:06:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "azurerm_virtual_network" "vnet" {
     name                  =   "${var.prefix}-vnet"
     resource_group_name   =   azurerm_resource_group.rg.name
     location              =   azurerm_resource_group.rg.location
-    address_space         =   var.vnet_address_range
+    address_space         =   [var.vnet_address_range["address_range"]]
 }
 
 #
