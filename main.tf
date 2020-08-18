4080aaf830c12637363cdff6b6001e0459875ea9
nandimandalamrajeshreddy <rajeshreddy.nandi@gmail.com>
2020-08-18 18:25:04 +0530

    removed map variables

--- main.tf
+++ main.tf
@@ -144,8 +144,8 @@ resource "azurerm_storage_account" "sa" {
     name                          =    "sa${random_string.sa_name.result}${count.index+1}"
     resource_group_name           =    azurerm_resource_group.rg.name
     location                      =    azurerm_resource_group.rg.location
-    account_tier                  =    var.saVars["account_tier"]
-    account_replication_type      =    var.saVars["account_replication_type"]
+    account_tier                  =    var.account_tier
+    account_replication_type      =    var.account_replication_type
 }
 
 
